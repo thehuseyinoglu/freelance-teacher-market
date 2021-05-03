49a288ee0d1f86cbb316360ea74c9f4a2c459634
152120171032 <thehuseyinoglu@gmail.com>
2021-05-03 16:21:38 +0300

    Create Job class

---
+++ models/job.js
@@ -0,0 +1,10 @@
+class Job {
+    constructor(teacher, title, content, price) {
+        this.teacher = teacher
+        this.title = title
+        this.content = content
+        this.price = price
+    }
+}
+
+module.exports = Job
