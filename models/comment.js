219953ce42129d5db4d29b76b800d4714b52388e
152120171032 <thehuseyinoglu@gmail.com>
2021-05-03 16:23:25 +0300

    Create Comment class

---
+++ models/comment.js
@@ -0,0 +1,10 @@
+class Comment {
+    constructor(teacher, student, text, score) {
+        this.teacher = teacher
+        this.student = student
+        this.text = text
+        this.score = score
+    }
+}
+
+module.exports = Comment
