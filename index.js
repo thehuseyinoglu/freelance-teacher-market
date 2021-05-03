dd9548a5db6f7b14277010eb55c7a78cf748b058
152120171032 <thehuseyinoglu@gmail.com>
2021-05-03 16:51:02 +0300

    Rebuild based on file-based database new file opened to create records

--- index.js
+++ index.js
@@ -1,38 +1,20 @@
-const colors = require('colors')
-const Student = require('./student')
-const Teacher = require('./teacher')
+const { printBookingHistory, printCommentHistory } = require('./lib/print-history')
+const { studentDatabase, teacherDatabase } = require('./database')
 
-function printBooking(booking) {
-    console.log(`${colors.red(booking.student.name)} booked ${colors.blue(booking.teacher.name)} to study ${colors.bgRed.white(booking.subject)} subject at ${colors.bgRed.white(booking.date)}`)
-}
+const dogukan = teacherDatabase.findBy('name', 'Dogukan')
+const berkay = studentDatabase.findByName('Berkay')
+const jobs = teacherDatabase.findByJob('Math Teacher')
 
-function printBookingHistory(student) {
-    student.bookings.forEach(printBooking)
-}
+berkay.book(dogukan, 'code review', '24.05.2021')
+studentDatabase.update(berkay)
 
-function printComment(comment) {
-    console.log(`${colors.red(comment.student.name)} commented ${colors.yellow(comment.text)} and gave ${colors.yellow(comment.score)} point`)
-}
+berkay.comment(dogukan, 'Nice lesson', 5)
+teacherDatabase.update(dogukan)
 
-function printCommentHistory(teacher) {
-    teacher.comments.forEach(printComment)
-}
-
-const berkay = new Student('Berkay', 22)
-const dogukan = new Teacher('Dogukan')
-
-dogukan.createJob("Math Teacher","Ozel ders",250)
-console.log(`Dogukan ilan: ${dogukan.job.title},${dogukan.job.content},${dogukan.job.price}TL `)
-
-berkay.book(dogukan, 'logaritma', '28.04.2021')
-berkay.book(dogukan, 'carpanlara ayirma', '30.04.2021')
-berkay.book(dogukan, 'ekspress', '29.04.2021')
-berkay.book(dogukan, 'code review', '01.05.2021')
-
-berkay.comment(dogukan, "nice lesson", 5)
-
-printBookingHistory(berkay)
 printCommentHistory(dogukan)
+printBookingHistory(berkay)
 
+berkay.follow(dogukan)
 
-
+console.log(jobs[0].name)
+console.log(berkay.following[0].name)
