3a1df4cbae4a60b7bbe84ece46c5e6b3ed19bb0c
152120171032 <thehuseyinoglu@gmail.com>
2021-05-03 16:23:50 +0300

    Create Booking class

---
+++ models/booking.js
@@ -0,0 +1,10 @@
+class Booking {
+    constructor(teacher, student, subject, date) {
+        this.teacher = teacher
+        this.student = student
+        this.subject = subject
+        this.date = date
+    }
+}
+
+module.exports = Booking
