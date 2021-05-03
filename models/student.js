151cdcbf5c77880bd23807bdbc719b78581e8606
152120171032 <thehuseyinoglu@gmail.com>
2021-05-03 16:19:40 +0300

    Create Student class and add create, follow functions

---
+++ models/student.js
@@ -0,0 +1,41 @@
+const Booking = require('./booking')
+const Comment = require('./comment')
+const uuid = require('uuid')
+
+class Student {
+    constructor(id = uuid.v4(), name, age, bookings = [], following = []) {
+        this.id = id
+        this.name = name
+        this.age = age
+        this.bookings = bookings
+        this.following = following
+    }
+
+    book(teacher, subject, date) {
+        const booking = new Booking(teacher, this, subject, date)
+
+        this.bookings.push(booking)
+
+        return booking
+    }
+
+    comment(teacher, text, score) {
+        const comment = new Comment(teacher, this, text, score)
+
+        teacher.comments.push(comment)
+
+        return comment
+    }
+
+    follow(teacher) {
+        teacher.followers.push(this)
+        this.following.push(teacher)
+    }
+
+    static create({ id, name, age, bookings, following }) {
+        return new Student(id, name, age, bookings, following)
+    }
+
+}
+
+module.exports = Student
