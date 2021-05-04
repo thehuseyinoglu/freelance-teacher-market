79376c088e4b5b67771aca15ba4d34db563f779e
152120171032 <thehuseyinoglu@gmail.com>
2021-05-04 14:15:35 +0300

    Add teacher bookings array and update create function

--- models/teacher.js
+++ models/teacher.js
@@ -2,20 +2,21 @@ const Job = require('./job')
 const uuid = require('uuid')
 
 class Teacher {
-    constructor(id = uuid.v4(), name, comments = [], job = '', followers = []) {
+    constructor(id = uuid.v4(), name, comments = [], job = '', followers = [], bookings = []) {
         this.id = id
         this.name = name
         this.comments = comments
         this.job = job
         this.followers = followers
+        this.bookings = bookings
     }
 
     createJob(title, content, price) {
         this.job = new Job(this, title, content, price)
     }
 
-    static create({ id, name, comments, job, followers }) {
-        return new Teacher(id, name, comments, job, followers)
+    static create({ id, name, comments, job, followers, bookings}) {
+        return new Teacher(id, name, comments, job, followers, bookings)
     }
 }
 
