d78a380c852ded8f20c083972796a45af83185ea
152120171032 <thehuseyinoglu@gmail.com>
2021-05-04 14:12:32 +0300

    Delete findByName function

--- database/student-database.js
+++ database/student-database.js
@@ -2,11 +2,7 @@ const BaseDatabase = require('./base-database')
 const Student = require('../models/student')
 
 class StudentDatabase extends BaseDatabase {
-    findByName(name) {
-        const objects = this.load()
 
-        return objects.find(o => o.name == name) //her bir eleaman object(o) o nun içinde arıyor
-    }
 }
 
 module.exports = new StudentDatabase(Student)
