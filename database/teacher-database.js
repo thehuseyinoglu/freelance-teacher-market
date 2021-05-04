f8e11aa363e3328b4883051c00eca264b8ad292c
152120171032 <thehuseyinoglu@gmail.com>
2021-05-04 14:13:50 +0300

    Delete findByTeacherName function

--- database/teacher-database.js
+++ database/teacher-database.js
@@ -2,15 +2,11 @@ const BaseDatabase = require('./base-database')
 const Teacher = require('../models/teacher')
 
 class TeacherDatabase extends BaseDatabase {
-    findByTeacherName(name) {
-        return this.findBy('name', name)
-    }
-
     findByJob(title) {
         const objects = this.load()
         objects.find(o => o.job.title == title)
-        return objects
 
+        return objects
     }
 }
 
