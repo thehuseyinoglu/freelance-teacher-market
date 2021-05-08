c1d8e336466efa640500cd404ed49f525ca72c4c
152120171032 <thehuseyinoglu@gmail.com>
2021-05-08 16:43:32 +0300

    Set create-records to async

--- create-records.js
+++ create-records.js
@@ -11,7 +11,6 @@ const samet = Teacher.create({ name: 'Samet' })
 
 dogukan.createJob("Math Teacher", "Ozel ders", 250)
 samet.createJob("Math Teacher", "Ozel ders", 100)
-teacherDatabase.save([dogukan, samet])
 
 berkay.book(dogukan, 'logaritma', '28.04.2021')
 berkay.book(dogukan, 'carpanlara ayirma', '30.04.2021')
@@ -20,17 +19,21 @@ berkay.book(dogukan, 'code review', '01.05.2021')
 mert.book(dogukan, 'node.js', '01.07.2021')
 
 mert.comment(dogukan, 'good lesson', 4)
-
 berkay.comment(dogukan, "nice lesson", 5)
 
-studentDatabase.save([berkay, mert])
-teacherDatabase.save([dogukan, samet])
+async function main() {
+    try {
+        await studentDatabase.save([berkay, mert])
+        await teacherDatabase.save([dogukan, samet])
 
-const betul = Student.create({ name: 'Betul', age: 12 })
+        const betul = Student.create({ name: 'Betul', age: 12 })
 
-studentDatabase.insert(betul)
+        await studentDatabase.insert(betul)
+        const students = await studentDatabase.load()
+        students.forEach(printBookingHistory)
+    } catch (e) {
+        return console.log(e)
+    }
+}
 
-const students = studentDatabase.load()
-
-students.forEach(printBookingHistory)
-
+main()
