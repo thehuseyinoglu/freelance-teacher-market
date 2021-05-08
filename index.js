e1a9f11fc53bb3037e3ebade184d7a7186a9850a
152120171032 <thehuseyinoglu@gmail.com>
2021-05-08 16:41:30 +0300

    Add the main function to index and make the function async

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const { printBookingHistory, printCommentHistory } = require('./lib/print-history')
 const { studentDatabase, teacherDatabase } = require('./database')
 
-const dogukan = teacherDatabase.findBy('name', 'Dogukan')
-const berkay = studentDatabase.findBy('name','Berkay')
-const jobs = teacherDatabase.findByJob('Math Teacher')
 
-berkay.book(dogukan, 'code review', '24.05.2021')
-studentDatabase.update(berkay)
+async function main() {
+    const dogukan = await teacherDatabase.findBy('name', 'Dogukan')
+    const berkay = await studentDatabase.findBy('name', 'Berkay')
+    const jobs = await teacherDatabase.findByJob('Math Teacher')
 
-berkay.comment(dogukan, 'Nice lesson', 5)
-teacherDatabase.update(dogukan)
+    berkay.book(dogukan, 'code review', '24.05.2021')
+    
+    berkay.comment(dogukan, 'Nice lesson', 5)
+    
+    await teacherDatabase.update(dogukan)
+    await studentDatabase.update(berkay)
 
-printCommentHistory(dogukan)
-printBookingHistory(berkay)
+    printCommentHistory(dogukan)
+    printBookingHistory(berkay)
 
-berkay.follow(dogukan)
+    berkay.follow(dogukan)
 
-console.log(jobs[0].name)
+    console.log(jobs[0].name)
+}
+
+main()
+
+
+
+
+
