707dd8ceac7e62819ea3ddd5bc2e7b8ddf5887dc
152120171032 <thehuseyinoglu@gmail.com>
2021-05-16 02:14:29 +0300

    Change file name  to fetch-records

---
+++ fetch-records.js
@@ -0,0 +1,29 @@
+const { printBookingHistory, printCommentHistory } = require('./lib/print-history')
+const { studentDatabase, teacherDatabase } = require('./database')
+
+async function main() {
+    const dogukan = await teacherDatabase.findBy('name', 'Dogukan')
+    const berkay = await studentDatabase.findBy('name', 'Berkay')
+    const jobs = await teacherDatabase.findByJob('Math Teacher')
+
+    berkay.book(dogukan, 'code review', '24.05.2021')
+    
+    berkay.comment(dogukan, 'Nice lesson', 5)
+    
+    berkay.follow(dogukan)
+
+    await teacherDatabase.update(dogukan)
+    await studentDatabase.update(berkay)
+
+    printCommentHistory(dogukan)
+    printBookingHistory(berkay)
+
+    console.log(jobs[0].name)
+}
+
+main()
+
+
+
+
+
